95a11f7a43bf1b01db0912dab2e6fe8b41b7f803
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-04 19:39:39 +0100

    OneOutOfTwoInOne -> .net framework 4.7.2, Test -> .net core

--- OneOutOfTwoInOne/Properties/AssemblyInfo.cs
+++ OneOutOfTwoInOne/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZeroOutOfZeroInOne")]
+[assembly: AssemblyTitle("OneOutOfTwoInOne472")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZeroOutOfZeroInOne")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("OneOutOfTwoInOne472")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25efd8f9-ea54-4d1e-af5d-c8949ae400df")]
+[assembly: Guid("3e24814c-bcb6-4560-a016-3ea281810efe")]
 
 // Version information for an assembly consists of the following four values:
 //

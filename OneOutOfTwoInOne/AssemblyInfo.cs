620049fce645480f21c403deb294a1c0559b46e3
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-05 07:11:57 +0100

    Stop testing .net framework 4.5 projects, migrate to 4.7.2 or .net core

--- OneOutOfTwoInOne/AssemblyInfo.cs
+++ OneOutOfTwoInOne/AssemblyInfo.cs
@@ -4,32 +4,29 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwoOutOfTwoInTwo.MoreTest")]
+[assembly: AssemblyTitle("Aspenlaub.Net.GitHub.CSharp.OneOutOfTwoInOne")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TwoOutOfTwoInTwo.MoreTest")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Aspenlaub.Net.GitHub.CSharp.OneOutOfTwoInOne")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee20bee8-d19b-42a0-9636-25854458f99b")]
+[assembly: Guid("201ad3fd-e148-416f-a2b1-6b317daf681c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
